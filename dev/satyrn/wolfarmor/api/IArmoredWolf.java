731473425f807e1dc75696c4e3b76e28c91d961d
satyrnidae <maskreybe@centurionfox.net>
2019-06-01 23:48:21 -0400

    :boom: Moved wolf armor item class to api

--- dev/satyrn/wolfarmor/api/IArmoredWolf.java
+++ dev/satyrn/wolfarmor/api/IArmoredWolf.java
@@ -1,4 +1,31 @@
 package dev.satyrn.wolfarmor.api;
 
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nonnull;
+
 public interface IArmoredWolf {
+
+    boolean getHasChest();
+
+    void setHasChest(boolean value);
+
+    boolean getHasArmor();
+
+    int getMaxSizeInventory();
+
+    void setArmorItemStack(@Nonnull ItemStack armorItemStack);
+
+    void equipArmor(@Nonnull ItemStack armorItemStack);
+
+    boolean canEquipItem(@Nonnull ItemStack armorItemStack);
+
+    void setInventoryItem(int index, @Nonnull ItemStack itemStack);
+
+    @Nonnull
+    ItemStack getArmorItemStack();
+
+    @Nonnull
+    InventoryBasic getInventory();
 }
---
+++ dev/satyrn/wolfarmor/api/util/DataHelper.java
@@ -0,0 +1,11 @@
+package dev.satyrn.wolfarmor.api.util;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.datasync.DataParameter;
+
+public abstract class DataHelper {
+
+    public static DataParameter<Boolean> HAS_CHEST;
+
+    public static DataParameter<ItemStack> ARMOR_ITEM;
+}
