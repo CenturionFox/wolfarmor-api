79ccb5d4f1623cd7ebc48a23a4ed1d9954393d37
Isabel Maskrey <isabel@satyrn.dev>
2020-08-14 21:01:58 -0400

    ✨ Hungry wolves IArmoredWolf now extends ICreatureFoodStats

--- src/main/java/dev/satyrn/wolfarmor/api/entity/passive/IArmoredWolf.java
+++ src/main/java/dev/satyrn/wolfarmor/api/entity/passive/IArmoredWolf.java
@@ -1,42 +1,136 @@
 package dev.satyrn.wolfarmor.api.entity.passive;
 
+import dev.satyrn.wolfarmor.api.entity.IFoodStatsCreature;
+import net.minecraft.inventory.IInventoryChangedListener;
 import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
-public interface IArmoredWolf {
+/**
+ * The main interface for all of the functionality added to the wolf entity by Wolf Armor and Storage
+ * @author Isabel Maskrey (satyrnidae)
+ * @version 2.3.0
+ */
+public interface IArmoredWolf extends IFoodStatsCreature, IInventoryChangedListener {
+    /**
+     * Gets the type of the wolf backpack as an item
+     * @return The chest type of the wolf backpack
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    @Nullable Item getChestType();
 
+    /**
+     * Sets the chest type of the wolf backpack to the specified item stack
+     * @param chestType An item stack which describes the backpack chest type
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    void setChestType(@Nonnull ItemStack chestType);
+
+    /**
+     * Checks if the wolf has a backpack
+     * @return <c>true</c> if the wolf has a backpack; otherwise, <c>false</c>
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     boolean getHasChest();
 
-    void setHasChest(boolean value);
+    /**
+     * Sets the wolf to either have or not have an backpack
+     * @param hasChest <c>true</c> to add a wolf backpack, <c>false</c> to remove it
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    void setHasChest(boolean hasChest);
 
-    void setChestType(@Nonnull ItemStack stack);
-
-    Item getChestType();
-
-    boolean getHasArmor();
-
+    /**
+     * Gets the maximum size of a wolf's inventory
+     * @return The maximum configured size of a wolf's inventory
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     int getMaxSizeInventory();
 
+    /**
+     * Gets the inventory instance
+     * @return The wolf's inventory instance
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    @Nonnull InventoryBasic getInventory();
+
+    /**
+     * Gets the armor item stack from the data manager
+     * @return The current armor item stack
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    @Nonnull ItemStack getArmorItemStack();
+
+    /**
+     * Sets the wolf's armor item stack in its data manager
+     * @param armorItemStack The armor item stack
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     void setArmorItemStack(@Nonnull ItemStack armorItemStack);
 
-    void equipArmor(@Nonnull ItemStack armorItemStack);
+    /**
+     * Checks if the wolf currently has armor equipped
+     * @return <c>true</c> if the armor item is set; otherwise, <c>false</c>
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    boolean getHasArmor();
 
+    /**
+     * Checks if a specific item stack can be equipped by the wolf as armor
+     * @param armorItemStack The armor item stack
+     * @return <c>true</c> if the armor item can be equipped; otherwise, <c>false</c>.
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     boolean canEquipItem(@Nonnull ItemStack armorItemStack);
 
+    /**
+     * Equips an item stack as armor, if possible
+     * @param armorItemStack The armor item stack to equip, provided canEquipItem is <c>true</c>
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    void equipArmor(@Nonnull ItemStack armorItemStack);
+
+    /**
+     * Sets the inventory item in the specified slot
+     * @param index The index of the slot to alter in the inventory
+     * @param itemStack The item stack to insert into the specified slot
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     void setInventoryItem(int index, @Nonnull ItemStack itemStack);
 
-    @Nonnull
-    ItemStack getArmorItemStack();
-
-    @Nonnull
-    InventoryBasic getInventory();
-
-    void dropInventoryContents();
-
+    /**
+     * Drops any equipment that the wolf is carrying or wearing
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     void dropEquipment();
 
+    /**
+     * Drops the wolf's backpack as a chest
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
     void dropChest();
+
+    /**
+     * Empties all items in the wolf's inventory into the world.
+     * @author Isabel Maskrey (satyrnidae)
+     * @since 1.0.0
+     */
+    void dropInventoryContents();
 }
